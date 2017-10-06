78298f7c415179e58aa5f603b9f9decd148ab0a3
Rose99Oliver <rose.99.oliver@gmail.com>
2017-10-05 23:13:17 -0400

    Add files via upload

---
+++ Madlibs/Madlibs/Mad.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	string word; 
+	string name; 
+	string story = "Once there was a ";
+	string charName;
+
+}
