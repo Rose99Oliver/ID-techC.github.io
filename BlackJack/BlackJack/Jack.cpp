cebeec33d1814218b2255f177bb01ebcf1aac224
Rose99Oliver <rose.99.oliver@gmail.com>
2017-10-05 22:57:43 -0400

    Add files via upload

---
+++ BlackJack/BlackJack/Jack.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include <string>
+#include <iostream> 
+#include <time.h> 
+using namespace std;
+
+int main()
+{
+	cout << " Black Jack" << endl << endl;
+
+
+	while (true)
+	{
+		int playerChoice;
+		cout << "1) to hit" << endl;
+		cout << "2) to stay" << endl;
+		
+		cin >> playerChoice;
+	
+		if (playerChoice = 1)
+		{
+			int computerChoice = rand() % 14;
+		}
+
+		else 	
+	}
+}
